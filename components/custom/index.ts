33c3a88d4c654fe224b10727b68bee7b8c608446
fantienan <363982607@qq.com>
2020-04-09 10:33:49 +0800

    添加Custom的扩展组件

--- components/custom/index.ts
+++ components/custom/index.ts
@@ -1,8 +1,22 @@
 // @ts-nocheck
-import Tree from './tree';
+import Form from './form'
+import Tree from './tree'
+import Table from './table'
+import SelectTree from './select-tree'
+import ImportFile from './import-file'
+import List from './list'
+import Upload from './upload'
+import UploadFile from './upload/upload-file'
 
 const Custom = {
-    Tree
+    Form,
+    Tree,
+    Table,
+    SelectTree,
+    ImportFile,
+    List,
+    Upload,
+    UploadFile,
 }
 
 export default Custom
